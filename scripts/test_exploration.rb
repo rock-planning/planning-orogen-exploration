1591c1025df377d7d0d1dec4be23f1b41b5953c9
Christian Clausen <cclausen@tzi.de>
2013-10-16 13:12:41 +0200

    testscript

---
+++ scripts/test_exploration.rb
@@ -0,0 +1,48 @@
+require 'orocos'
+require 'vizkit'
+require 'readline'
+
+Orocos::CORBA.max_message_size = 12000000 # stucks if > than this
+
+include Orocos
+Orocos.initialize
+
+# Content of these language variables must not be german, otherwise '.' and ','
+# are mixed reading numbers and a bad_alloc error occurs loading the scenes.
+ENV['LANG'] = 'C'
+ENV['LC_NUMERIC'] = 'C'
+
+Orocos.run 'envire::SynchronizationTransmitter' => 'transmitter', 
+        'corridor_planner::Traversability' => 'traversability',
+        'exploration::Task' => 'exploration',
+        "wait" => 1000 do
+        
+    #Orocos.conf.load_dir("./config")
+    
+    # LOAD ENV AND CREATE TRAV
+    # What is 'Orocos.name_service.get' instead of 'TaskContext::get'
+    transmitter = TaskContext::get 'transmitter'
+    transmitter.configure()
+    transmitter.start()
+    
+    traversability = Orocos.name_service.get 'traversability'
+    traversability.apply_conf(['default'])
+    traversability.configure()
+    traversability.start()
+    
+    exploration = TaskContext::get 'exploration'
+    exploration.configure()
+    exploration.start()
+    # CONNECT PORTS
+    transmitter.envire_events.connect_to(traversability.mls_map)
+
+    # LOAD MAP
+    transmitter.loadEnvironment("#{ENV['AUTOPROJ_PROJECT_BASE']}/bundles/spacebot/data/traversability_maps/dlr.env")
+    
+    #while true
+        transmitter.loadEnvironment("#{ENV['AUTOPROJ_PROJECT_BASE']}/bundles/spacebot/data/traversability_maps/dlr.env")
+    #end
+   
+    Readline::readline("Press ENTER to exit ...")
+end
+
