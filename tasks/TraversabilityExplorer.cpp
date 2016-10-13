82ade03c8e3507d5958179ba077b480bb375d189
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2016-10-13 14:20:45 +0200

    Exploration working (without goal heading though).

--- tasks/TraversabilityExplorer.cpp
+++ tasks/TraversabilityExplorer.cpp
@@ -1,14 +1,17 @@
 #include "TraversabilityExplorer.hpp"
 
+#include <envire/Orocos.hpp>
+#include <envire/maps/TraversabilityGrid.hpp>
+
 using namespace exploration;
 
 TraversabilityExplorer::TraversabilityExplorer(std::string const& name)
-    : TraversabilityExplorerBase(name)
+	: TraversabilityExplorerBase(name)
 {
 }
 
 TraversabilityExplorer::TraversabilityExplorer(std::string const& name, RTT::ExecutionEngine* engine)
-    : TraversabilityExplorerBase(name, engine)
+	: TraversabilityExplorerBase(name, engine)
 {
 }
 
@@ -18,35 +21,112 @@ TraversabilityExplorer::~TraversabilityExplorer()
 
 bool TraversabilityExplorer::configureHook()
 {
-    if (! TraversabilityExplorerBase::configureHook())
-        return false;
-    return true;
+	if (! TraversabilityExplorerBase::configureHook())
+		return false;
+	return true;
 }
 
 bool TraversabilityExplorer::startHook()
 {
-    if (! TraversabilityExplorerBase::startHook())
-        return false;
-    return true;
+	if (! TraversabilityExplorerBase::startHook())
+		return false;
+	return true;
 }
 
 void TraversabilityExplorer::updateHook()
 {
-    TraversabilityExplorerBase::updateHook();
+	TraversabilityExplorerBase::updateHook();
+	
+	// Read in the new map
+	envire::OrocosEmitter::Ptr binary_event;
+	while(_envire_map.read(binary_event) == RTT::NewData)
+	{
+		mEnvironment.applyEvents(*binary_event);   
+	}
+	std::vector<envire::TraversabilityGrid*> maps = mEnvironment.getItems<envire::TraversabilityGrid>();
+	if(maps.size() == 0)
+	{
+		LOG_ERROR("Environment has no traversability map!");
+		return;
+	}
+	if(maps.size() > 1)
+	{
+		LOG_WARN("The environment contains more than one traversability map, will use the first!");
+	}
+	envire::TraversabilityGrid* trav = maps[0];
+	envire::TraversabilityGrid::ArrayType& trav_array = maps[0]->getGridData();
+	size_t size_x = trav->getCellSizeX();
+	size_t size_y = trav->getCellSizeY();
+	GridMap gridMap(size_x, size_y);
+	GridPoint point;
+	for(size_t y = 0; y < size_y; y++)
+	{
+		point.y = y;
+		for (size_t x = 0; x < size_x; x++)
+		{
+			point.x = x;
+			if(trav->getProbability(x,y) < 0.5)
+				gridMap.setData(point, UNKNOWN);
+			else if(trav->getTraversability(x,y).getDrivability() <= 0)
+				gridMap.setData(point, OBSTACLE);
+			else
+				gridMap.setData(point, VISIBLE);
+		}
+	}
+	
+	// Debug output map
+	envire::Environment tr;
+	envire::TraversabilityGrid* exploreMap = mPlanner.coverageMapToTravGrid(gridMap, *trav);
+	envire::FrameNode *fr = new envire::FrameNode(trav->getFrameNode()->getTransform());
+	tr.getRootNode()->addChild(fr);
+	tr.attachItem(exploreMap, fr);
+	exploreMap->setFrameNode(fr);
+	envire::OrocosEmitter emitter(&tr, _explore_map);
+	emitter.setTime(base::Time::now());
+	emitter.flush(); 
+	
+	// Read in start point
+	base::samples::RigidBodyState rbs;
+	while(_robot_pose.read(rbs) == RTT::NewData)
+	{
+	}
+	
+	size_t gridx, gridy;
+	if(trav->toGrid(rbs.position[0],rbs.position[1],gridx,gridy))
+	{
+		GridPoint start(gridx, gridy, 0);
+		PointList front_cells = mPlanner.getFrontierCells(&gridMap, start, true);
+		if(front_cells.empty())
+		{
+			LOG_ERROR("%s", mPlanner.getStatusMessage());
+			return;
+		}
+		GridPoint goal = front_cells.at(0);
+		double posex, posey;
+		trav->fromGrid(goal.x, goal.y, posex, posey);
+		
+		rbs.position[0] = posex;
+		rbs.position[1] = posey;
+		rbs.time = base::Time::now();
+		_goal_pose.write(rbs);
+	}else
+	{
+		LOG_ERROR("Start point is not in map.");
+	}
 }
 
 void TraversabilityExplorer::errorHook()
 {
-    TraversabilityExplorerBase::errorHook();
+	TraversabilityExplorerBase::errorHook();
 }
 
 void TraversabilityExplorer::stopHook()
 {
-    TraversabilityExplorerBase::stopHook();
+	TraversabilityExplorerBase::stopHook();
 }
 
 void TraversabilityExplorer::cleanupHook()
 {
-    TraversabilityExplorerBase::cleanupHook();
+	TraversabilityExplorerBase::cleanupHook();
 }
 
--- tasks/TraversabilityExplorer.hpp
+++ tasks/TraversabilityExplorer.hpp
@@ -3,25 +3,30 @@
 
 #include "exploration/TraversabilityExplorerBase.hpp"
 
+#include <envire/core/Environment.hpp>
+#include <exploration/Planner.hpp>
+
 namespace exploration
 {
-    class TraversabilityExplorer : public TraversabilityExplorerBase
-    {
+	class TraversabilityExplorer : public TraversabilityExplorerBase
+	{
 	friend class TraversabilityExplorerBase;
-    protected:
+	protected:
+		envire::Environment mEnvironment;
+		Planner mPlanner;
 
-    public:
-        TraversabilityExplorer(std::string const& name = "exploration::TraversabilityExplorer");
-        TraversabilityExplorer(std::string const& name, RTT::ExecutionEngine* engine);
-        ~TraversabilityExplorer();
+	public:
+		TraversabilityExplorer(std::string const& name = "exploration::TraversabilityExplorer");
+		TraversabilityExplorer(std::string const& name, RTT::ExecutionEngine* engine);
+		~TraversabilityExplorer();
 
-        bool configureHook();
-        bool startHook();
-        void updateHook();
-        void errorHook();
-        void stopHook();
-        void cleanupHook();
-    };
+		bool configureHook();
+		bool startHook();
+		void updateHook();
+		void errorHook();
+		void stopHook();
+		void cleanupHook();
+	};
 }
 
 #endif
